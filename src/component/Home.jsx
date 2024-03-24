36234e048a8de299db183690cc9f1cb371b9f50e
Muhammad Talha Shaikh <m-talha-shaikh@protonmail.com>
2024-03-25 01:07:13 +0500

    load image issue

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -10,27 +10,27 @@ export default function Home() {
 			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
 			<div id="categories">
 				<div class="category-item">
-					<img src="./pizza-icon.png" alt="" />
+					<img src="/assets/pizza-icon.png" alt="" />
 					<h3>Breakfast</h3>
 				</div>
 				<div class="category-item">
-					<img src="./pizza-icon.png" alt="" />
+					<img src="/assets/pizza-icon.png" alt="" />
 					<h3>Lunch</h3>
 				</div>
 				<div class="category-item">
-					<img src="./pizza-icon.png" alt="" />
+					<img src="/assets/pizza-icon.png" alt="" />
 					<h3>Dinner</h3>
 				</div>
 				<div class="category-item">
-					<img src="./pizza-icon.png" alt="" />
+					<img src="/assets/pizza-icon.png" alt="" />
 					<h3>Party</h3>
 				</div>
 				<div class="category-item">
-					<img src="./pizza-icon.png" alt="" />
+					<img src="/assets/pizza-icon.png" alt="" />
 					<h3>Dessert</h3>
 				</div>
 				<div class="category-item">
-					<img src="./pizza-icon.png" alt=""  />
+					<img src="/assets/pizza-icon.png" alt=""  />
 					<h3>Smoothie</h3>
 				</div>
 			</div>
