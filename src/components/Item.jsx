8a4b6baadd178d2040a89e3f57126a27e94fd172
Muhammad Talha Shaikh <m-talha-shaikh@protonmail.com>
2024-03-28 00:10:40 +0500

    fix background

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -6,7 +6,7 @@ const Item = ({ data, title, image, onBackClick }) => {
   return (
     <div className='recipe-item'>
       <button style={{ color: 'white', backgroundColor: 'lightskyblue', margin: '10px auto', padding: '10px', fontSize: '30px', borderRadius: '5px' }} onClick={onBackClick}>Go Back</button>
-      <h2 style={{ display: 'inline-block', backgroundColor: 'black' , padding: '20px', borderRadius: '5px' , color: 'white', fontSize: '40px'}}>{title}</h2>
+      <h2 style={{ display: 'inline-block', backgroundColor: 'black' , padding: '20px', borderRadius: '5px' , color: 'white', fontSize: '30px'}}>{title}</h2>
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} id="image-item">
         <img src={image} alt={title} style={{ display: 'inline-block' , width: '40vw' , height: 'auto'}} />
       </div>
